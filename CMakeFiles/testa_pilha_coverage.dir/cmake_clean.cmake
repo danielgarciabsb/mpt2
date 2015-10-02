f3f1ea2ab86efed899e2795005f108a97c3d0b31
Daniel Garcia <danielgarciabsb@gmail.com>
2015-10-01 23:32:43 -0300

    Adaptado pilha vetor com teste - limpo

--- CMakeFiles/testa_pilha_coverage.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/testa_pilha_coverage"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/testa_pilha_coverage.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
--- pilha_vetor/CMakeFiles/pilha_vetor.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-FILE(REMOVE_RECURSE
-  "libpilha_vetor.a"
-)
